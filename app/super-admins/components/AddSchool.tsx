dd134f03ebd9f70f28433e2c6268554e131bac73
Mubashir Iqbal <m.iqbal29.work@gmail.com>
2026-03-04 00:48:40 +0530

    feat: add prisma setup and initial schema

---
+++ app/super-admins/components/AddSchool.tsx
@@ -0,0 +1,7 @@
+export default function AddSchool() {
+    return (
+        <div>
+            <h1>Add School</h1>
+        </div>
+    );
+}
---
+++ app/super-admins/page.tsx
@@ -0,0 +1,10 @@
+import AddSchool from "./components/AddSchool";
+
+export default function SuperAdminsPage() {
+    return (
+        <div>
+            <h1>Super Admins</h1>
+            <AddSchool />
+        </div>
+    );
+}
